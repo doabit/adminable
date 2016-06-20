50ee0dd4bda8db2d548ad36f5255212210d34229
Sean Deng <doinsist@gmail.com>
2016-06-20 11:27:44 +0800

    Fix #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Adminable::Engine.routes.draw do
   Adminable.resources.each do |r|
-    resources r.route, except: :show, path: r.name, controller: r.name
+    resources r.route.to_sym, except: :show, path: r.name, controller: r.name
   end
 
   root to: redirect(Adminable::Configuration.redirect_root_path)
